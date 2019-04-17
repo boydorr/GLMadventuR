fd88e7f06cfcb48b9b474fc511109d437b6e1598
Claire Harris <c.harris.2@research.gla.ac.uk>
2019-04-17 16:41:16 +0100

    VisualiseData: Fix bugs with column naming.

--- R/VisualiseData/server.R
+++ R/VisualiseData/server.R
@@ -4,15 +4,15 @@ library("cowplot")
 
 function(input, output) {
   dragons <- read.csv("../DataGeneration/dragons.csv")
+  response <- colnames(dragons)[2]
   output$plot = renderPlot({
-    response <- colnames(dragons)[1]
     if (input$variable_selection == "hunting"){
       ggplot(data = dragons) +
         geom_boxplot(aes_string(y = response, x = input$variable_selection)) +
-        xlab(response) + ylab(input$variable_selection)
+        ylab(response) + xlab(input$variable_selection)
     }else{
     ggplot(data = dragons) +
       geom_point(aes_string(y = response, x = input$variable_selection)) +
-      xlab(response) + ylab(input$variable_selection)}
+      ylab(paste(response)) + xlab(input$variable_selection)}
   })
 }
