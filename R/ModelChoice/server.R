78efec90af6df58c875263a2f73860adb0bb688e
dpascall <david.pascall@mac.com>
2019-04-17 19:01:20 +0100

    End of day commit, NHST complete

--- R/ModelChoice/server.R
+++ R/ModelChoice/server.R
@@ -5,13 +5,21 @@ function(input, output) {
   output$modelcommand = renderText({
     if (is.null(input$variable_selection)){
       vars <- 1
-    }else{
+    } else {
       vars <- paste(input$variable_selection, collapse = " + ")
     }
     response <- colnames(dragons[,1]
     formula <- paste0("glm(", response, " ~ ", vars, 
                       ", family = 'gaussian', data = dragons)")
-    summary(eval(parse(text=formula)))
+    if (input$model_selection=="NHST") {
+      pvalueoutput<-summary(eval(parse(text=formula)))$coefficients[,c(1,4)]
+      row.names(pvalueoutput)<-vars
+      pvalueoutput
+    } elseif (input$model_selection=="AIC") {
+      
+    } else {
+    
+    }
   })
 }
 
