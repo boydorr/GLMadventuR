5be1dcc61c7b87a8702d32fe630ebae3658c6b41
dpascall <david.pascall@mac.com>
2019-04-18 10:38:24 +0100

    Version for testing by other team members

--- R/ModelChoice/server.R
+++ R/ModelChoice/server.R
@@ -1,20 +1,28 @@
 library("shiny")
+library("xtable")
 
 function(input, output) {
-  output$modelcommand = renderText({
+  dragons<-read.csv("~/Desktop/git/GLMadventuR/R/DataGeneration/dragons.csv")
+  output$modelcommand = renderTable({
     if (is.null(input$variable_selection)){
       vars <- 1
     } else {
       vars <- paste(input$variable_selection, collapse = " + ")
     }
-    response <- colnames(dragons[,1]
+    response <- colnames(dragons)[1]
     formula <- paste0("glm(", response, " ~ ", vars, 
                       ", family = 'gaussian', data = dragons, na.action=na.fail)")
     if (input$model_selection=="NHST") {
-      pvalueoutput<-summary(eval(parse(text=formula)))$coefficients[,c(1,4)]
-      row.names(pvalueoutput)<-vars
-      pvalueoutput
-    } elseif (input$model_selection=="AIC") {
+      if (is.null(input$variable_selection)){
+      
+      } else {
+        as.matrix(summary(eval(parse(text=formula)))$coefficients)
+        # pvalueoutput<-summary(eval(parse(text=formula)))$coefficients[,c(1,4)]
+        # pvalueoutput
+        # row.names(pvalueoutput)<-vars
+        # pvalueoutput
+      }
+    } else if (input$model_selection=="AIC") {
       MuMIn::dredge(eval(parse(text=formula)))
     } else {
       print("Not yet implemented.")
--- R/ModelChoice/ui.R
+++ R/ModelChoice/ui.R
@@ -6,20 +6,21 @@ fluidPage(
   sidebarLayout(
     sidebarPanel(
       radioButtons("model_selection",
-                         label = "Choose your model selection method",
-                         choiceNames = c("Null Hypothesis Significance Testing (HHST)",
-                                         "Akaike's Information Criterion (AIC)",
-                                         "Elastic Net"),
-                         choiceValues = c("NHST","AIC","EN")
+                   label = "Choose your model selection method",
+                   choiceNames = c("Null Hypothesis Significance Testing (NHST)",
+                                   "Akaike's Information Criterion (AIC)",
+                                   "Elastic Net"),
+                   choiceValues = c("NHST","AIC","EN")),
       checkboxGroupInput("variable_selection",
                          label = "Choose the most complex model",
                          choiceValues = c("sheep", "hunting",
                                           "unicorn", "vegetation_height"),
                          choiceNames = c("Sheep", "Hunting",
-                                          "Unicorn", "Vegetation Height"))
+                                         "Unicorn", "Vegetation Height"))
     ),
     ## Show a plot of the generated distribution
     mainPanel(textOutput("modelcommand"))
   )
 )
+
 
