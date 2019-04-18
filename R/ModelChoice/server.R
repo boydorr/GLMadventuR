4431d0f693bb00b38b54b5c981a3b54bce17b159
dpascall <david.pascall@mac.com>
2019-04-18 14:24:00 +0100

    Fixed bug where function call was '-eval' rather than 'eval'

--- R/ModelChoice/server.R
+++ R/ModelChoice/server.R
@@ -1,9 +1,10 @@
 library("shiny")
 library("xtable")
+library("glmnet")
 
 function(input, output) {
   ##read in data
-  dragons<-read.csv("~/Desktop/git/GLMadventuR/R/DataGeneration/dragons.csv")
+  dragons <- read.csv("~/Desktop/git/GLMadventuR/R/DataGeneration/dragons.csv")
   ##render table
   output$modelcommand = renderTable({
     ##generate list for correct glm formlula from inputted variables
@@ -18,7 +19,7 @@ function(input, output) {
     formula <- paste0("glm(", response, " ~ ", vars,
                       ", family = '",input$distribution_choice,"', data = dragons(), na.action=na.fail)")
     ##run command from string
-    dragonmodel<--eval(parse(text=formula))
+    dragonmodel <- eval(parse(text=formula))
     ##if NHST
     if (input$model_selection=="NHST") {
       ##currently no output if no options selected -- what would make sense?
@@ -43,19 +44,19 @@ function(input, output) {
       x <- as.matrix(dragons()[,colnames(dragons())%in%input$variable_selection])
       y <- dragons()[,1]
       ##cross validate to optimise alpha and lambda
-      lambdaforseqalong<-exp(seq(from=-10, to=0, by=0.1))
-      optim<-matrix(NA,nrow=101,ncol=101)
-      alphasforseqalong<-seq(from=0, to=1, by=0.01)
+      lambdaforseqalong <- exp(seq(from=-10, to=0, by=0.1))
+      optim <- matrix(NA,nrow=101,ncol=101)
+      alphasforseqalong <- seq(from=0, to=1, by=0.01)
       for (i in 1:101){
-        optim[,i]<-cv.glmnet(x,y,nfolds=nrow(x),lambda = lambdaforseqalong,alpha=alphasforseqalong[i])$cvm
+        optim[,i] <- cv.glmnet(x,y,nfolds=nrow(x),lambda = lambdaforseqalong,alpha=alphasforseqalong[i])$cvm
       }
       ##run model at optimal parameter values
-      dragonmodel<-glmnet(x, y, family=input$distribution_choice, lambda=lambdaforseqalong[which(optim == min(optim), arr.ind=TRUE)[1]],
+      dragonmodel <- glmnet(x, y, family=input$distribution_choice, lambda=lambdaforseqalong[which(optim == min(optim), arr.ind=TRUE)[1]],
              alpha=alphasforseqalong[which(optim == min(optim), arr.ind=TRUE)[2]])
       ##generate matrix and print
-      dragonmatrix<-as.matrix(dragonmodel$beta)
-      row.names(dragonmatrix)<-vars
-      colnames(dragonmatrix)<-"Coefficients"
+      dragonmatrix <- as.matrix(dragonmodel$beta)
+      row.names(dragonmatrix) <- vars
+      colnames(dragonmatrix) <- "Coefficients"
       dragonmatrix
     }
   }, include.rownames=T)
