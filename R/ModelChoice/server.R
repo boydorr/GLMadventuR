5e26eb59fabb46302af6d52eb202ae7fb82a9b6f
dpascall <david.pascall@mac.com>
2019-04-18 12:34:30 +0100

    Working model selection code for NHST and AIC - ready for implementation in main app

--- R/ModelChoice/server.R
+++ R/ModelChoice/server.R
@@ -2,31 +2,44 @@ library("shiny")
 library("xtable")
 
 function(input, output) {
+  ##read in data
   dragons<-read.csv("~/Desktop/git/GLMadventuR/R/DataGeneration/dragons.csv")
+  ##render table
   output$modelcommand = renderTable({
+    ##generate list for correct glm formlula from inputted variables
     if (is.null(input$variable_selection)){
       vars <- 1
     } else {
       vars <- paste(input$variable_selection, collapse = " + ")
     }
+    ##set response variable - will need changing when feedthrough implemented
     response <- colnames(dragons)[1]
-    formula <- paste0("glm(", response, " ~ ", vars, 
-                      ", family = 'gaussian', data = dragons, na.action=na.fail)")
+    ##glm command as string
+    formula <- paste0("glm(", response, " ~ ", vars,
+                      ", family = '",input$distribution_choice,"', data = dragons, na.action=na.fail)")
+    ##run command from string
+    dragonmodel<--eval(parse(text=formula))
+    ##if NHST
     if (input$model_selection=="NHST") {
+      ##currently no output if no options selected -- what would make sense?
       if (is.null(input$variable_selection)){
-      
+
       } else {
-        as.matrix(summary(eval(parse(text=formula)))$coefficients)
-        # pvalueoutput<-summary(eval(parse(text=formula)))$coefficients[,c(1,4)]
-        # pvalueoutput
-        # row.names(pvalueoutput)<-vars
-        # pvalueoutput
+        ##correct test depends on family so generate p-values based on F-test for Gaussian
+        ##and Chi-squared for Poisson
+        if (input$distribution_choice=="gaussian") {
+          as.matrix(drop1(dragonmodel,test="F")[c(2:nrow(drop1(testglm,test="F"))),])
+        } else if (input$distribution_choice=="poisson") {
+          as.matrix(drop1(dragonmodel,test="Chisq")[c(2:nrow(drop1(testglm,test="Chisq"))),])
+        }
       }
+    ##if using information criteria - just give dredge output
     } else if (input$model_selection=="AIC") {
-      MuMIn::dredge(eval(parse(text=formula)))
+      dredged<-MuMIn::dredge(eval(parse(text=formula)))
+      dredged
+    ##glmnet elastic net not implemented yet
     } else {
-      print("Not yet implemented.")
     }
-  })
+  }, include.rownames=T)
 }
 
--- R/ModelChoice/ui.R
+++ R/ModelChoice/ui.R
@@ -2,15 +2,16 @@ library("shiny")
 
 fluidPage(
   titlePanel("Model selection"), #title
-  ## Sidebar with variable selection and model selection method
   sidebarLayout(
     sidebarPanel(
+      ##choose model selection method
       radioButtons("model_selection",
                    label = "Choose your model selection method",
                    choiceNames = c("Null Hypothesis Significance Testing (NHST)",
                                    "Akaike's Information Criterion (AIC)",
                                    "Elastic Net"),
                    choiceValues = c("NHST","AIC","EN")),
+      ##choose variables
       checkboxGroupInput("variable_selection",
                          label = "Choose the most complex model",
                          choiceValues = c("sheep", "hunting",
@@ -18,8 +19,8 @@ fluidPage(
                          choiceNames = c("Sheep", "Hunting",
                                          "Unicorn", "Vegetation Height"))
     ),
-    ## Show a plot of the generated distribution
-    mainPanel(textOutput("modelcommand"))
+    ##output table
+    mainPanel(tableOutput("modelcommand"))
   )
 )
 
